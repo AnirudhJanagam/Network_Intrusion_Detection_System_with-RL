7f897167c5c0e68a1da7bda8186cf71ec252f2a9
Anirudh Janagam <anirudh.goku0@gmail.com>
2018-12-20 15:56:24 +0100

    Create Classifiers.py

---
+++ NIDS/Classifiers.py
@@ -0,0 +1,13 @@
+import gym
+from keras import optimizers
+
+class Classififer:
+    def __init__(self):
+        pass
+
+    def get_classifier(self):
+        '''
+            returns Classifier object
+        '''
+        clf = SGDClassifier()
+return clf
